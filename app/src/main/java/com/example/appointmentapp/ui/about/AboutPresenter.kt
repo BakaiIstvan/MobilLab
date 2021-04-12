81b85de6d74f6b49ef70a68d315fc18839f1170c
BakaiIstvan <bakai.istvan.lfgeb@gmail.com>
2021-04-12 08:02:15 +0200

    Added presenter and screen for about

---
+++ app/src/main/java/com/example/appointmentapp/ui/about/AboutPresenter.kt
@@ -0,0 +1,6 @@
+package com.example.appointmentapp.ui.about
+
+import com.example.appointmentapp.ui.Presenter
+
+class AboutPresenter : Presenter<AboutScreen?>() {
+}
---
+++ app/src/main/java/com/example/appointmentapp/ui/about/AboutScreen.kt
@@ -0,0 +1,4 @@
+package com.example.appointmentapp.ui.about
+
+interface AboutScreen {
+}
