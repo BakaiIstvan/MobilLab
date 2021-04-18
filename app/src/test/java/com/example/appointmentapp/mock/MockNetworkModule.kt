0a4ea41c57d73e83b8b753d742a76a0083c1a18d
BakaiIstvan <bakai.istvan.lfgeb@gmail.com>
2021-04-18 22:32:13 +0200

    Observable -> Call Started mock classes implementations

---
+++ app/src/test/java/com/example/appointmentapp/mock/MockNetworkModule.kt
@@ -0,0 +1,21 @@
+package com.example.appointmentapp.mock
+
+import com.example.appointmentapp.network.AppointmentsAPI
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import javax.inject.Singleton
+
+class MockNetworkModule {
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        return OkHttpClient.Builder().addInterceptor(interceptor).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideAppointmentsApi(): AppointmentsAPI = MockAppointmentsAPI()
+}
