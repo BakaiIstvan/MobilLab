d5d95a00ef83de4ed44801e8b44cfc4acd8a9e38
BakaiIstvan <bakai.istvan.lfgeb@gmail.com>
2021-05-02 23:06:27 +0200

    Added saveAppointment test to NewAppointmentTest

--- app/src/main/java/com/example/appointmentapp/ui/newappointment/NewAppointmentScreen.kt
+++ app/src/main/java/com/example/appointmentapp/ui/newappointment/NewAppointmentScreen.kt
@@ -3,8 +3,8 @@ package com.example.appointmentapp.ui.newappointment
 import com.example.appointmentapp.model.Appointment
 
 interface NewAppointmentScreen {
-    fun loadAppointment(appointment: Appointment)
-    fun showAppointmentSaved(id: String)
-    fun showAppointmentModified(id: String)
+    fun loadAppointment(appointment: Appointment?)
+    fun showAppointmentSaved(id: String?)
+    fun showAppointmentModified(id: String?)
     fun showError(errorMsg: String)
 }
--- app/src/test/java/com/example/appointmentapp/test/NewAppointmentTest.kt
+++ app/src/test/java/com/example/appointmentapp/test/NewAppointmentTest.kt
@@ -1,4 +1,58 @@
 package com.example.appointmentapp.test
 
+import com.example.appointmentapp.model.Appointment
+import com.example.appointmentapp.model.AppointmentBody
+import com.example.appointmentapp.testInjector
+import com.example.appointmentapp.ui.appointment.AppointmentScreen
+import com.example.appointmentapp.ui.newappointment.NewAppointmentPresenter
+import com.example.appointmentapp.ui.newappointment.NewAppointmentScreen
+import com.example.appointmentapp.utils.argumentCaptor
+import com.example.appointmentapp.utils.mock
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito
+import org.robolectric.RobolectricTestRunner
+import java.math.BigDecimal
+import javax.inject.Inject
+
+@RunWith(RobolectricTestRunner::class)
 class NewAppointmentTest {
+    @Inject
+    lateinit var newAppointmentPresenter: NewAppointmentPresenter
+
+    private lateinit var newAppointmentScreen: NewAppointmentScreen
+
+    @Before
+    @Throws(Exception::class)
+    fun setup() {
+        testInjector.inject(this)
+        newAppointmentScreen = mock()
+        newAppointmentPresenter.attachScreen(newAppointmentScreen)
+    }
+
+    @Test
+    fun testSaveAppointment() {
+        val appointmentBody = AppointmentBody(APPOINTMENT_TITLE, APPOINTMENT_START, APPOINTMENT_END, APPOINTMENT_DURATION, APPOINTMENT_DESCRIPTION)
+        newAppointmentPresenter.saveAppointment(appointmentBody)
+
+        val item = argumentCaptor<String>()
+        Mockito.verify(newAppointmentScreen).showAppointmentSaved(item.capture())
+        assert(item.value == APPOINTMENT_ID)
+    }
+
+    @After
+    fun tearDown() {
+        newAppointmentPresenter.detachScreen()
+    }
+
+    companion object {
+        private const val APPOINTMENT_ID = "id1"
+        private const val APPOINTMENT_TITLE = "Appointment1"
+        private const val APPOINTMENT_START = "2021-06-01T14:45:00.000Z"
+        private const val APPOINTMENT_END = "2021-06-01T14:45:00.000Z"
+        private val APPOINTMENT_DURATION = BigDecimal(30)
+        private const val APPOINTMENT_DESCRIPTION = "Same as last time"
+    }
 }
